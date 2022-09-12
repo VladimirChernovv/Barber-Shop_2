feb90943ce718aca1b89b3d0891417d75f11d48b
Vladimir Chernov <v.chernov@zebrains.team>
2022-09-12 10:01:50 +0300

    1. After the user typed and submit his data on the Visit page(visit.erb) they will be recording in a file ntebook.txt(.publick/notebook.txt). 2. Created file(views/notification) whin open when user pushe the button(Sign up - on Visit page(visit.erb)). 3. Added notebook.txt into .gitignore

--- app.rb
+++ app.rb
@@ -14,6 +14,21 @@ get '/visit' do
   erb :visit
 end
 
+post '/visit' do
+  @user_name = params[:user_name]
+  @user_phone_number = params[:user_phone_number]
+  @datetime = params[:datetime]
+
+  @notification = "Thank you."
+  @notification_title = "Dear #{@user_name} we'll be wating for you at #{@datetime}."
+
+  notebook = File.open './publick/notebook.txt', 'a'
+  notebook.write "User: #{@user_name}, Phone: #{@user_phone_number}, Date: #{@datetime}\n"
+  notebook.close
+
+  erb :notification
+end
+
 get '/contacts' do
   erb :contacts
 end
